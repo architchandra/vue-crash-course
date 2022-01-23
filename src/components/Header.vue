ae9317388f43c6d93fd3e385678aa31031448ebc
Archit Chandra <architchandra@gmail.com>
2022-01-23 20:10:16 +0530

    Add Add Task button to Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,22 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
+    <Button text="Add Task" color="blue" />
   </header>
 </template>
 
 
 
 <script>
+  import Button from './Button';
+
   export default {
     name: 'Header',
     props: {
       title: String,
+    },
+    components: {
+      Button
     },
   };
 </script>

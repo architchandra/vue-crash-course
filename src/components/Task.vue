e9d0ab9eaff1b050435178f602f09be281a505a5
Archit Chandra <architchandra@gmail.com>
2022-01-24 21:32:38 +0530

    Simplify delete task code

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="[task.reminder ? 'reminder' : '', 'task']">
     <h3>{{ task.text }}
-      <i @click="onDelete(task.id)" class="fas fa-times"></i>
+      <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
     </h3>
     <p>{{ task.day }}</p>
   </div>
@@ -14,11 +14,6 @@
     name: 'Task',
     props: {
       task: Object,
-    },
-    methods: {
-      onDelete(id) {
-        this.$emit('delete-task', id);
-      },
     },
   };
 </script>

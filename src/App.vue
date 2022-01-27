068632372d97fb9e65e7445e5fb919b1a746c12c
Archit Chandra <architchandra@gmail.com>
2022-01-27 13:02:38 +0530

    Add proxy to mask localhost route on dev

--- src/App.vue
+++ src/App.vue
@@ -45,13 +45,13 @@
         );
       },
       async fetchTasks() {
-        const res = await fetch('http://localhost:5000/tasks');
+        const res = await fetch('api/tasks');
         const data = await res.json();
         
         return data;
       },
       async fetchTask(id) {
-        const res = await fetch(`http://localhost:5000/tasks/${id}`);
+        const res = await fetch(`api/tasks/${id}`);
         const data = await res.json();
         
         return data;

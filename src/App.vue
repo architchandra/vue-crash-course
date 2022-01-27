cde023c6492a88cf11b36814b8fb22330ed6f01f
Archit Chandra <architchandra@gmail.com>
2022-01-27 13:32:45 +0530

    Allow tasks to be added to json-server

--- src/App.vue
+++ src/App.vue
@@ -30,8 +30,17 @@
       toggleAddTask() {
         this.showAddTask = !this.showAddTask;
       },
-      addTask(task) {
-        this.tasks = [...this.tasks, task];
+      async addTask(task) {
+        const res = await fetch('api/tasks', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(task),
+        });
+        const data = await res.json();
+        
+        this.tasks = [...this.tasks, data];
       },
       deleteTask(id) {
         if (confirm('Are you sure?')) {
